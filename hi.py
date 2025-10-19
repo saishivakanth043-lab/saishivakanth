fd50f96a97eeedffae58a04ce30eb20435c1ac1f
saishivakanth043-lab <saishivakanth043@gmail.com>
2025-10-19 11:01:29 +0530

    Add files via upload

---
+++ hi.py
@@ -0,0 +1,18 @@
+import tkinter as tk
+from tkinter import colorchooser
+
+def change_background_color():
+    """Opens a color chooser dialog and changes the window background color."""
+    color_code = colorchooser.askcolor(title="Choose color")
+    if color_code:
+     root.configure(bg=color_code[1])
+
+root = tk.Tk()
+root.title("hello sai")
+
+root.geometry("400x300")
+change_color_button = tk.Button(root, text="Change Background Color", command=change_background_color)
+change_color_button.pack(pady=50)
+
+
+root.mainloop()
